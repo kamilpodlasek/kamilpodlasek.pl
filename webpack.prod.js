c01105492b8cfc7295aa834c0e012a2f8adce06c
Kamil Podlasek <kamil.podlasek@gmail.com>
2023-07-05 13:37:54 +0200

    Tweak Webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,17 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
     },
-    plugins: [new CleanWebpackPlugin(['dist'])],
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new CopyPlugin({
+            patterns: [{ from: 'portfolio-projects', to: 'portfolio-projects' }],
+        }),
+    ],
 });
